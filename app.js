e7bf1e4d625f9adfc24c94694545602d284da447
SUSHMITHA.SK <51911682+sushmitha1109@users.noreply.github.com>
2019-10-30 14:57:22 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
 var p1 = document.getElementById("player1").addEventListener("click", addscore1);
 var p2 = document.getElementById("player2").addEventListener("click", addscore2);
-// var reset = document.getElementById("reset").addEventListener("click", resetgame);
+var reset = document.getElementById("reset").addEventListener("click", resetgame);
 var p1score= document.querySelector("#p1score")
 var p2score= document.querySelector("#p2score")
 var score=0 , score1=0;
-var gameover=false;
+var gameover = false;
 var win=5;
  function addscore1()
     {
         if(!gameover){
         score++; 
        if(p1score===win)
-       gameover=true;
+       { 
+       gameover = true;
        }
-       p1score.textContent=score;
+       p1score.textContent = score;
     }
-    
+   };  
 
  function addscore2()
- {
+ {  
+    if(!gameover){
     score1++; 
+    if(p1score===win)
+    {
+       gameover = true;
+    }
     p2score.textContent=score1;
  }
+};
 
-//  function resetgame()
-//  {
-//     document.getElementById("p1score").reset();
-//     document.getElementById("p2score").reset();
+ function resetgame()
+ {
+   p1score.textContent = 0;
+   p2score.textContent = 0;    
     
-//  }
+ };
